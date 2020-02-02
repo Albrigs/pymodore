16cf53fb4f510b8b0394a0fc8bfea707adfdcc80
NatanFernandesSantos <natanfernandessantos@protonmail.com>
2020-02-02 17:55:34 -0300

    adicionando cores

--- main.py
+++ main.py
@@ -31,13 +31,13 @@ def pomodoro():
     task = "🍅 None"
     if state['cur'] == 'task':
         state['rounds'] += 1
-        task = "🍅 Begin:"+ datetime.now().strftime("%d/%m/%Y %H:%M:%S") + ' | ' + input('What is the current task?')
+        task = "🍅 Begin:"+ datetime.now().strftime("%d/%m/%Y %H:%M:%S") + ' | ' + input(colored('What is the current task?\n','green'))
 
-    input('Press ENTER to start the timer. 🍅')
-    print("Seconds are bad ヽ(ಠ_ಠ)ノ")
+    input('Press {0} to start the timer. {1}'.format(colored('ENTER','green'), colored('🍅', 'red')))
+    print("Seconds are bad "+ colored('ヽ(ಠ_ಠ)ノ', 'red')) if state['cur'] == 'task'
 
     for i in range(cfg[state['cur']]):
-        time_left = '\rMinutes Left: ' + str(cfg[state['cur']] -i)
+        time_left = '\rMinutes Left: ' + colored(str(cfg[state['cur']] -i),'blue')
 
         sys.stdout.write(time_left) #escrevendo
         sys.stdout.flush() #atualizando
@@ -46,28 +46,27 @@ def pomodoro():
     # fim do timer
     for e in range(3):
         sound.play()
-        sleep(1)
+        sleep(60)
 
     if state['cur'] == 'task':
         log = open('pymodore_log.txt', 'a')
-        log.write('\n' + task)
+        # log.write('\n' + task)
         log.close()
         if state['rounds'] == cfg['max_rounds']:
             state['cur'] = 'big_rest'
-            message = "\nTime for a big rest! Press ENTER and take 30 mins! ('N' to quit)"
+            message = "\nTime for a big rest! Press {0} and take 30 mins! {1}".format(colored('ENTER','green'), colored("('N' to quit)",'magenta'))
 
         else:
             state['cur'] = 'rest'
-            message = "\nStart rest? ('N' to quit)"
+            message = "\nStart rest? {0} to continue. {1}".format(colored('ENTER','green'), colored("('N' to quit)",'magenta'))
 
     else:
         state['cur'] = 'task'
-        message = "\nStart new task? ('N' to quit)"
-
+        message = "\nStart new task? {0} to continue. {1}".format(colored('ENTER','green'), colored("('N' to quit)",'magenta'))
 
     progress = input(message)
-    if progress.lower() == 'n' or progress.lower() == 'no':
-        return print('Remember to check your log')
+    if progress.strip().lower()[0] == 'n':
+        return print(colored('Remember to check your log', 'red'))
 
     pomodoro()
 
